bbe4a7bb16c2ce993d06a5bb619dc7a5bfc05c72
Malpel <m.a.pelkonen2@gmail.com>
2017-11-28 12:26:45 +0200

    started project

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template
+
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    flask_app.run(debug=True)
