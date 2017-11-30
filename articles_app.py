2c312eb822e1d970a15d2c227d97896e95d579b9
Malpel <m.a.pelkonen2@gmail.com>
2017-11-30 18:49:15 +0200

    registration working

--- articles_app.py
+++ articles_app.py
@@ -8,6 +8,17 @@ from passlib.hash import sha256_crypt
 
 
 articles_app = Flask(__name__)
+
+#Config MariaDB
+articles_app.config["MYSQL_HOST"] = "localhost"
+articles_app.config["MYSQL_USER"] = "root"
+articles_app.config["MYSQL_PASSWORD"] = "mariadpulmunen"
+articles_app.config["MYSQL_DB"] = "articles"
+articles_app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+#init MariaDB
+mariaDb = MySQL(articles_app)
+
 Articles = Articles()
 
 @articles_app.route("/")
@@ -31,16 +42,35 @@ class RegisterForm(Form):
     username = StringField("Username", [validators.Length(min=1, max=25)])
     email = StringField("Email", [validators.Email(message="Please provide a legit email address")])
     password = PasswordField("Password", [validators.DataRequired(), validators.EqualTo("confirm", message="Passwords don't match")])
-    confirm = PasswordField("Confrim password")
+    confirm = PasswordField("Confirm password")
 
 @articles_app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm(request.form)
+
     if request.method == "POST" and form.validate():
-        return render_template("register.html")
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #Cursor
+        cur = mariaDb.connection.cursor()
+        
+        cur.execute("INSERT INTO User(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #Commit to database
+        mariaDb.connection.commit()
+
+        cur.close()
+
+        flash("Register successful! Sign into your account", "success")
+        
+        return redirect(url_for("index"))
     return render_template("register.html", form=form)
 
 
 
 if __name__ == '__main__':
+    articles_app.secret_key="secret123"
     articles_app.run(debug=True)
