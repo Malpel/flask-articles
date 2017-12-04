16f564ec71f1bc6657e55269dd2da60c4b272286
Malpel <m.a.pelkonen2@gmail.com>
2017-12-04 12:56:44 +0200

    added login and session stuff

--- articles_app.py
+++ articles_app.py
@@ -5,6 +5,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 
 articles_app = Flask(__name__)
@@ -12,7 +13,7 @@ articles_app = Flask(__name__)
 #Config MariaDB
 articles_app.config["MYSQL_HOST"] = "localhost"
 articles_app.config["MYSQL_USER"] = "root"
-articles_app.config["MYSQL_PASSWORD"] = "mariadpulmunen"
+articles_app.config["MYSQL_PASSWORD"] = "*********"
 articles_app.config["MYSQL_DB"] = "articles"
 articles_app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
@@ -27,7 +28,7 @@ def index():
 
 @articles_app.route("/articles")
 def articles():
-    return render_template("articles.html", articles = Articles)
+    return render_template("articles.html", articles=Articles)
 
 @articles_app.route("/articles/<string:id>/")
 def article(id):
@@ -69,7 +70,57 @@ def register():
         return redirect(url_for("index"))
     return render_template("register.html", form=form)
 
+@articles_app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password_candidate = request.form["password"]
 
+        cur = mariaDb.connection.cursor()
+        result = cur.execute("SELECT * FROM User WHERE username = %s", [username])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data["password"]
+
+            if sha256_crypt.verify(password_candidate, password):
+                session["logged_in"] = True
+                session["username"] = username
+
+                flash("You are now logged in", "success")
+                return redirect(url_for("dashboard"))
+            else:
+                error = "Invalid login"
+            return render_template("login.html", error=error)
+        
+            cur.close()
+
+        else:
+            error = "Username not found"
+            return render_template("login.html", error=error)
+
+    return render_template("login.html")
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Unauthorized", "danger")
+            return redirect(url_for("login"))
+    return wrap
+
+@articles_app.route("/dashboard")
+@is_logged_in
+def dashboard():
+    return render_template("dashboard.html")
+
+@articles_app.route("/logout")
+def logout():
+    session.clear()
+    flash("You are now logged out", "success")
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     articles_app.secret_key="secret123"
